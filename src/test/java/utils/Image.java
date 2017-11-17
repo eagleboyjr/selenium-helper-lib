576098a26ac9d9cc193d60789ef3d2c49e1c65d5
Todd Eagle <todd.eagle@gmail.com>
2017-11-17 01:23:12 -0500

    Added Image Utilities

--- src/test/java/utils/Image.java
+++ src/test/java/utils/Image.java
@@ -5,22 +5,43 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 import static utils.UtilConstants.PROJECT_NAME;
 
 public class Image {
 
     public void takeScreenshot(WebDriver driver, String path) throws IOException {
-        String fileName = PROJECT_NAME + Randomize.randomNum() + ".png";
-        String compPath = path + "/" + fileName;
 
+        String fileName = PROJECT_NAME + Randomize.randomNum() + ".png";
+        String fullPath =concatAndPathAndFile(fileName, path);
         File sourceFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(sourceFile, new File(compPath));
+        FileUtils.copyFile(sourceFile, new File(fullPath));
     }
 
-    public void createImageFile(){
+    public void createImageFile(String path, int fileSize, String fileType ) throws IOException {
+
+        String fileName = PROJECT_NAME +  Randomize.randomNum() + ".pgm";
+        String completePath = concatAndPathAndFile(fileName, fileName);
+        File file = new File(completePath);
+
+        OutputStream out = new BufferedOutputStream(new FileOutputStream(file));
+        out.write("P5\n".getBytes(StandardCharsets.UTF_8));
+        out.write("255\n".getBytes(StandardCharsets.UTF_8));
+
+        for (int i = 0; i < fileSize; i++) {
+            out.write(0);
+        }
+
+        if(!fileType.equals("pgm")){ convertFile(completePath, fileType);}
+    }
+
+    private String concatAndPathAndFile(String file, String filePath){
+         return filePath + "/" + file;
+    }
+
+    public void convertFile(String fullPath, String imageType){
 
     }
 }
